5f7668331fcf85969274d7c70713e079249eda9e
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2025-04-27 23:19:24 -0400

    Add All data from backend

--- natures/src/ui/Tours.jsx
+++ natures/src/ui/Tours.jsx
@@ -2,14 +2,51 @@ import { useLoaderData } from "react-router-dom";
 import { getTours } from "../services/apiTours";
 
 function Tours() {
-    const tour = useLoaderData();
-    console.log(tour);
-    return <div>Tours</div>;
+    const tours = useLoaderData();
+    console.log(tours.data);
+    const data = tours.data;
+    return (
+        <ul>
+            {data.map((tour) => (
+                <TourItem tour={tour} key={tour.id} />
+            ))}
+        </ul>
+    );
 }
 
 export async function loader() {
-    const tour = await getTours();
-    return tour;
+    const tours = await getTours();
+    return tours;
+}
+
+function TourItem({ tour }) {
+    const {
+        name,
+        description,
+        ratingsAverage,
+        imageCover,
+        difficulty,
+        price,
+        summary
+    } = tour;
+
+    return (
+        <li>
+            <img
+                src={`/tours/${imageCover}`}
+                alt={name}
+                style={{ width: "100%", height: "auto" }}
+            />
+            <div>
+                <h2>{name}</h2>
+                <p>{description}</p>
+                <p>Rating: {ratingsAverage}</p>
+                <p>Difficulty: {difficulty}</p>
+                <p>Price: ${price}</p>
+                <p>{summary}</p>
+            </div>
+        </li>
+    );
 }
 
 export default Tours;
